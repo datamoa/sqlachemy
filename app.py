de403335fb90922ae25def642618d6cc46667b45
datamoa <irisshowy@gmail.com>
2019-07-30 19:52:37 -0700

    added hm dir

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 
+prcp_dict = [
+    {'2017-08-23': 0.45,
+  '2017-08-22': 0.5,
+  '2017-08-21': 0.56,
+  '2017-08-20': "nan"
+}
+]
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Home page menu"
+    return (
+        f"Welcome to Home page!<br />"
+        f"Available Routes:<br />"
+        f"/api/v1.0/precipitation<br />"
+        f"/api/v1.0/stations<br />"
+        f"/api/v1.0/tobs<br />"
+        f"/api/v1.0/<start><br />"
+        f"/api/v1.0/<start>/<end><br />"
+    )
 
-@app.route("/")
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    return jsonify(prcp_dict)
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#Database setup
+engine =create_engine('sqlite:///Resources/hawaii.sqlite')
+
+# reflect an existing database into a new model
+Base = automap_base()
+#reflect the tables
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+app= Flask(__name__)
+
+@app.route("/")
+def index():
+    return (
+        f"Welcome to Home page!<br />"
+        f"Available Routes:<br />"
+        f"/api/v1.0/precipitation<br />"
+        f"/api/v1.0/stations<br />"
+        f"/api/v1.0/tobs<br />"
+        f"/api/v1.0/&lt;start&gt;<br />"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br />"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+     """Return a list of percipitatipn data """
+     #Query all prcp data
+     session = Session(engine)
+     results = session.query(Measurement.date, Measurement.prcp).all()
+
+     # Create a dictionary from the row data and append to a list
+     precipitation = []
+     for date, prcp in results:
+         precipitation_dict ={}
+         precipitation_dict["Date"] = date
+         precipitation_dict["Precipitation"] = prcp
+         precipitation.append(precipitation_dict)
+     return jsonify(precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    #Query stations list from dataset
+     session = Session(engine)
+     names = session.query(Measurement.station).group_by(Measurement.station).all()
+
+    # Create a dictionary from the row data and append to a list
+     stat_names = []
+     for station in names:
+         stat_dict ={}
+         stat_dict["Station"] = station
+         stat_names.append(stat_dict)
+     return jsonify(stat_names)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+     session = Session(engine)
+     temp = session.query(Measurement.date, Measurement.tobs ).filter(Measurement.date >= '2016-08-23').\
+        order_by(Measurement.date.desc()).all()
+
+     temp_dict = []
+     for tobs in temp:
+         temperature ={}
+         temperature["Temperature"] = tobs
+         temp_dict.append(temperature)
+     return jsonify(temp_dict)
+
+@app.route("/api/v1.0")
+def start_date():
+     session = Session(engine)
+     max_temp = [func.max(Measurement.tobs),
+           func.min(Measurement.tobs),
+           func.avg(Measurement.tobs)]
+     stat_data = session.query(*max_temp).\
+         filter(Measurement.date >= '2016-08-23').all()
+     return jsonify(stat_data)
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
